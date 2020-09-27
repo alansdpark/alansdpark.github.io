a02e7067f0e473798814c76547fb4d766e26b1bf
Alan Park <alansdpark@gmail.com>
2020-09-27 02:30:05 -0700

    Updates

---
+++ precache-manifest.7366f2858f275a5be5de5f69aa5e498f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88aeed2733c88eb12dce5bc62acc8eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82841a859d9c705cbbf4",
+    "url": "/static/css/main.2c4ba28f.chunk.css"
+  },
+  {
+    "revision": "dc288c2bf775ccd450f1",
+    "url": "/static/js/2.57cd96c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.57cd96c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82841a859d9c705cbbf4",
+    "url": "/static/js/main.f4e38794.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "dfd192bbc8fc6852b3f20344511eea4c",
+    "url": "/static/media/SenjuPrac.dfd192bb.PNG"
+  },
+  {
+    "revision": "57c21f252d74abac5ada5b1e2859f6e4",
+    "url": "/static/media/SunsetHeader.57c21f25.JPG"
+  },
+  {
+    "revision": "7f060fd712f2ebbb4abd079d012799d4",
+    "url": "/static/media/code-24px.7f060fd7.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1015dd31ca8e243d0025ab03799b5306",
+    "url": "/static/media/plantsrobots.1015dd31.png"
+  },
+  {
+    "revision": "1a1199fa4e8131cb3042f0f08e128c15",
+    "url": "/static/media/stickytongue.1a1199fa.png"
+  },
+  {
+    "revision": "0c7a696f04feb94d89f680032d5bfd79",
+    "url": "/static/media/traveleasii.0c7a696f.png"
+  },
+  {
+    "revision": "58dc162734d7707cc9e3954241c286a9",
+    "url": "/static/media/verified-24px.58dc1627.svg"
+  }
+]);
